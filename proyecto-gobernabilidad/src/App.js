d88540096c1f2e915db4df47580e6965340f06d7
aisac712 <isaac.diaz@unitec.edu>
2023-06-15 23:14:19 -0600

    frontend listo

--- proyecto-gobernabilidad/src/App.js
+++ proyecto-gobernabilidad/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [textIssue, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(email+"\n"+textIssue)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="bg text-center">
+
+        {/*Div for Centered Text & Input*/}
+        <div class="centered">
+
+          <p class="firstLine"> R &nbsp; E &nbsp; P &nbsp; O &nbsp; R &nbsp; T &nbsp; . &nbsp; M &nbsp; E </p>
+
+          <p class="secondLine">Report an Issue</p>
+
+          <form onSubmit={handleSubmit}>
+            <p> <input class="InputStyle" type="email" id="emailLogin" placeholder='Email' required
+              value={email} onChange={(e) => setEmail(e.target.value)} /> </p>
+
+            <p><textarea class="InputStyle" id="textIssue" placeholder="I found a bug..." required
+              value={textIssue} onChange={(e) => setText(e.target.value)} /></p>
+
+            <button class="btn btn-primary" type="submit" value="Submit">Submit</button>
+          </form>
+
+        </div>
+      </div>
     </div>
   );
 }
