0e8ebf3debf6a67a15461a1a8174f02437e34977
aisac712 <isaac.diaz@unitec.edu>
2023-06-17 11:03:45 -0600

    https resquests done, api connected to frontend. everything works fine

--- proyecto-gobernabilidad/src/App.js
+++ proyecto-gobernabilidad/src/App.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [email, setEmail] = useState('');
-  const [textIssue, setText] = useState('');
+  const [subject, setSubject] = useState('');
+  const [description, setDescription] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(email+"\n"+textIssue)
+    const timestamp = new Date().toLocaleString();
+    //alert(email + " " + timestamp + "\n" + subject + "\n" + description);
+
+    const requestBody = `
+      <issue>
+        <subject>${subject}, sent by ${email} at ${timestamp}</subject>
+        <description>${description}</description>
+      </issue>
+    `;
+
+    try {
+      const response = await fetch('https://npr3s.com/control/projects/gti-1265-1167/issues.xml?key=5c57fde2d7058e78a578c071058c1ab85db870a3', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml',
+        },
+        body: requestBody
+      });
+
+      if (response.ok) {
+        Swal.fire("Issue reported", "Thank you!", "success");
+        // Reset form fields
+        setSubject('');
+        setDescription('');
+      } else {
+        Swal.fire(response.status, "Error creating issue", "error");
+      }
+    } catch (error) {
+      Swal.fire(error, "Error creating issue", "error");
+    }
   };
+
+
 
   return (
     <div>
@@ -26,8 +59,11 @@ function App() {
             <p> <input class="InputStyle" type="email" id="emailLogin" placeholder='Email' required
               value={email} onChange={(e) => setEmail(e.target.value)} /> </p>
 
+            <p> <input class="InputStyle" type="text" id="emailLogin" placeholder='Subject' required
+              value={subject} onChange={(e) => setSubject(e.target.value)} /> </p>
+
             <p><textarea class="InputStyle" id="textIssue" placeholder="I found a bug..." required
-              value={textIssue} onChange={(e) => setText(e.target.value)} /></p>
+              value={description} onChange={(e) => setDescription(e.target.value)} /></p>
 
             <button class="btn btn-primary" type="submit" value="Submit">Submit</button>
           </form>
